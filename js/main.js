145bdf22710684244ab221c06dd2f3e7fa4f37d1
lauratreg <treganowanl@gmail.com>
2017-03-21 15:04:32 +0700

    cssstyles

--- js/main.js
+++ js/main.js
@@ -17,4 +17,30 @@ $(document).ready(function(){
       owl.trigger('stop.owl.autoplay')
   })
 
+  $('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+
+      duration: 8000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    });
+
+
+
+  });
+
 });
